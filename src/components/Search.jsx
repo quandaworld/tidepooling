82cda6f3df9381e885b30b70c75ecb680377d08d
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-02-20 00:10:46 -0800

    Add search panel layout

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import banner from '/assets/banner.jpeg'
+import { IoSearchOutline } from "react-icons/io5";
 
 function Search() {
   return (
-    <div>Search</div>
+    <div className='flex justify-center flex-col'>
+      <img src={banner} className='rounded-md'/>
+      <div className='flex items-center gap-2 bg-white shadow-lg p-3 rounded-md mt-[-20%] mx-[20%]'>
+        <IoSearchOutline className='text-[20px] text-gray-500'/>
+        <input 
+          type='text' 
+          placeholder='Search tide levels by zip' 
+          className='outline-none'
+        />
+      </div>
+    </div>
   )
 }
 

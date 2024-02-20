599a8a425f58d0cffefaf25e130aa4d2eea47065
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-02-20 00:10:00 -0800

    Add header and navbar layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='flex items-center justify-between pt-5 pb-6'>
+      <h1 className='text-3xl md-text-4xl font-bold text-sky-700'>tidepooling.com</h1>
+      <ul className='md:flex gap-4 md:gap-20'>
+        <li className='hover:text-sky-700 font-semibold cursor-pointer'>Home</li>
+        <li className='hover:text-sky-700 font-semibold cursor-pointer'>Find Pools</li>
+        <li className='hover:text-sky-700 font-semibold cursor-pointer'>Talk Pools</li>
+        <li className='hover:text-sky-700 font-semibold cursor-pointer'>Log In</li>
+      </ul>
+      <button
+        className='text-center px-5 py-1 bg-sky-700 w-max font-medium text-white rounded-md cursor-pointer'
+      >
+        Sign Up
+      </button>
+    </div>
   )
 }
 

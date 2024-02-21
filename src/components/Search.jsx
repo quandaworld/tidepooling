fead24cf5f8528d5be6bcd27c5e76f81edbe99ae
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-02-21 00:43:08 -0800

    Update type animation speed

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -6,7 +6,7 @@ import { TypeAnimation } from 'react-type-animation';
 function Search() {
   return (
     <div className='flex relative justify-center flex-col'>
-      <div className='flex flex-col md:gap-4 text-center text-cyan-950 font-bold bg-white/40 absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-2/3 z-10 w-full md:mt-5 md:p-5'>
+      <div className='flex flex-col md:gap-4 text-center font-bold text-cyan-950 bg-white/40 absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-2/3 z-10 w-full md:mt-5 md:p-5'>
         <h1 className='lg:text-5xl md:text-2xl text-lg'>Low tides today at</h1>
         <h1 className='lg:text-7xl md:text-4xl text-xl'>
           <TypeAnimation
@@ -21,7 +21,7 @@ function Search() {
               3000
             ]}
             wrapper="span"
-            speed={50}
+            speed={200}
             repeat={Infinity}
           />
         </h1>

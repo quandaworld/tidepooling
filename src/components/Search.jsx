598229ddd9b042ae558fc6806b59a425e1707471
quandaworld <46980777+quandaworld@users.noreply.github.com>
2024-02-20 17:38:28 -0800

    Update headline styling

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -5,32 +5,33 @@ import { TypeAnimation } from 'react-type-animation';
 
 function Search() {
   return (
-    <div className='flex justify-center flex-col'>
-      <h1 className='absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-11/12 z-10 lg:text-7xl md:text-4xl text-xl font-bold text-white [text-shadow:1px_3px_2px_var(--tw-shadow-color)] shadow-slate-500'>
-        Low tides today at 
-        <TypeAnimation 
-          sequence={[
-            'La Jolla Beach',
-            2000, 
-            'Cabrillo National Monument',
-            2000,
-            'Duxbury Reef Reserve',
-            2000,
-            'Shi Shi Beach',
-            2000
-          ]}
-          wrapper="span"
-          speed={40}
-          className='lg:pl-5 pl-1.5'
-          repeat={Infinity}
-        />
-      </h1>
+    <div className='flex relative justify-center flex-col'>
+      <div className='flex flex-col md:gap-4 text-center text-cyan-950 font-bold bg-white/40 absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-2/3 z-10 w-full md:mt-5 md:p-5'>
+        <h1 className='lg:text-5xl md:text-2xl text-lg'>Low tides today at</h1>
+        <h1 className='lg:text-7xl md:text-4xl text-xl'>
+          <TypeAnimation
+            sequence={[
+              'La Jolla Beach',
+              3000,
+              'Cabrillo National Monument',
+              3000,
+              'Duxbury Reef Reserve',
+              3000,
+              'Shi Shi Beach',
+              3000
+            ]}
+            wrapper="span"
+            speed={50}
+            repeat={Infinity}
+          />
+        </h1>
+      </div>
       <img src={banner} className='rounded-md'/>
-      <div className='flex items-center gap-2 bg-white/80 shadow-lg shadow-slate-800 p-3 rounded-md mt-[-20%] mx-[20%]'>
-        <IoSearchOutline className='text-[20px] text-gray-500'/>
+      <div className='flex items-center text-xl gap-2 bg-white/80 shadow-lg shadow-slate-800 p-3 rounded-md mt-[-15%] mx-[20%]'>
+        <IoSearchOutline className='text-[22px] text-gray-500'/>
         <input 
           type='text' 
-          placeholder='Search tide levels by zip' 
+          placeholder='Search tide levels by zip code' 
           className='outline-none bg-transparent placeholder-gray-500'
         />
       </div>
